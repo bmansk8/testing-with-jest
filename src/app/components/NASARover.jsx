46c79f2be90cdc9488843a1fce7dd70005ee6a9c
bmansk8 <bmansk8@yahoo.com>
2020-04-11 10:38:06 -0400

    fixed routes
    fixed my nested routes. now i need to display data.

--- src/app/components/NASARover.jsx
+++ src/app/components/NASARover.jsx
@@ -4,7 +4,8 @@ import {
   Link,
   Route,
   useRouteMatch,
-  useParams
+  useParams,
+  Switch
 } from 'react-router-dom';
 
 //export const NASARover = function NASARover() {
@@ -35,35 +36,48 @@ export const NASARover = function NASARover() {
   //mapping through imgs and displaying them in imgs tags
   return (
     <div>
-      <h1 id='roverH1' className='text-center '>Photos from mars</h1>
-      <div id='rover-photos'>
-        {data.map((obj, index) => (
-          <div className='card' key={index}>
-            <img alt={obj.img_src} className='imgs shadow-lg rounded' src={obj.img_src}></img>
-            <div className='card-body'>
-              <h5 className="card-title">{obj.rover.name} Rover</h5>
-              <Link to={`${url}/${obj.id}`} size='sm' className="btn btn-primary">More Details.</Link>
-            </div>
+      <Switch>
+        <Route exact path={path}>
+          <h1 id='roverH1' className='text-center '>Photos from mars</h1>
+          <div id='rover-photos'>
+            {data.map((obj, index) => (
+              <div className='card' key={index}>
+                <img alt={obj.img_src} className='imgs shadow-lg rounded' src={obj.img_src}></img>
+                <div className='card-body'>
+                  <h5 className="card-title">{obj.rover.name} Rover</h5>
+                  {/*so i am looping through data and making cards for the photos
+                  if you see what nasa returns, they send a ton of stuff including a id
+                  so I deconstruct path and url so the path is relative
+                  then i send obj.id as part of the url
+                */}
+                  <Link to={`${url}/${obj.id}`} size='sm' className="btn btn-primary">More Details.</Link>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-
-      <Route path={`${path}/:photoId`}>
+        </Route>
+        {/*then use /:photoId as my placeHolder */}
+        <Route path={`${path}/:photoId`}>
           <Detailed />
-      </Route>
+        </Route>
+      </Switch>
 
     </div>
   );
 }
 
-function Detailed(){
+function Detailed() {
+  console.log(useParams());
 
-  let { topicId } = useParams();
+  var { photoId } = useParams();
 
-  return(
+  console.log(photoId);
+  //then retrieve it from useParams using deconstructing
+  return (
     <div>
-      <h2>{topicId}</h2>
+      <h2>{photoId}</h2>
       <p>hi! i am rendering</p>
+      <Link to='/quotes' size='sm' className="btn btn-primary" >Back</Link>
     </div>
   )
 }
